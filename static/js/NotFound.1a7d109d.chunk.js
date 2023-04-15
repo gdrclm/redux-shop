973fa624119efe55331d466ef3e4741c873cf70d
Danil Anisin <d.anisin97@gmail.com>
2023-04-16 00:48:22 +0300

    Updates

---
+++ static/js/NotFound.1a7d109d.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkredux_shop=self.webpackChunkredux_shop||[]).push([[187],{4949:function(e,n,s){s.r(n);var u=s(6662),r=s(184);n.default=function(){return(0,r.jsxs)(r.Fragment,{children:[(0,r.jsx)(u.Z,{}),(0,r.jsx)("button",{children:"\u043d\u0430\u0437\u0430\u0434"}),";"]})}}}]);
+//# sourceMappingURL=NotFound.1a7d109d.chunk.js.map
